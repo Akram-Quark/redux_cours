71b7434989bdc602915402cbc69f494e8e14f5d1
Bediaf Akram <88899765+Akram-Quark@users.noreply.github.com>
2021-12-24 00:01:46 +0100

    ok

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
 const redux = require("redux");
+const combineReducers = redux.combineReducers;
 function bake() {
   return {
     type: "BAKE_CAKE",
   };
 }
+function sel() {
+  return {
+    type: "sold_cakes",
+  };
+}
 const initState = {
-  numberOfCakes: 0,
+  numberOfCakesbaked: 0,
+};
+const saoldinit = {
+  numberOfCakesold: 0,
 };
 const reducer = (state = initState, action) => {
   if (action.type === "BAKE_CAKE") {
     return {
       ...state,
-      numberOfCakes: state.numberOfCakes + 1000,
+      numberOfCakesbaked: state.numberOfCakesbaked + 1000,
     };
   } else {
     return state;
   }
 };
+const soldreducer = (state = saoldinit, action) => {
+  if (action.type === "sold_cakes") {
+    return {
+      ...state,
+      numberOfCakesold: state.numberOfCakesold + 2,
+    };
+  } else {
+    return state;
+  }
+};
+let rootReducer = combineReducers({
+  reducer,
+  soldreducer,
+});
 const setStore = redux.createStore;
-let store = setStore(reducer);
-console.log("state A :", store.getState());
-const unsubscribe = store.subscribe(() =>
-  console.log(`ubdated state ${i}`, store.getState()),
-);
-let i = 0;
-while (i < 10) {
-  store.dispatch(bake());
-  i++;
-}
-unsubscribe();
+let store = setStore(rootReducer);
+store.getState();
+let unsub = store.subscribe(() => console.log(store.getState()));
+store.dispatch(bake());
+store.dispatch(sel());
+unsub();
